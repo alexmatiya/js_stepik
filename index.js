2ff74a729342147ebd6954c26175fff447b4e9d5
Alex Matiya <matiacka@yandex.ru>
2023-12-04 21:56:28 +0300

    Основы JavaScript

---
+++ index.js
@@ -0,0 +1,51 @@
+// let x = 80;
+
+// let y = 100;
+
+// console.log(x, y); // вывод значений x и y через пробел
+
+// console.log(x + y); // вывод результата сложения значений x и y 
+
+// console.log( "xxx" * 2 ); // Выведет NaN
+
+// console.log(2 > 1); // Выведет: true
+// console.log(9 < 3); // Выведет: false
+
+// let num;
+// console.log(num); // Выведет: undefined
+
+// let age = null;
+// console.log(age);
+
+
+// let username = prompt("Как вас зовут?");
+
+
+// const readline = require('readline').createInterface({
+
+//     input: process.stdin,
+    
+//     output: process.stdout
+    
+//     });
+    
+//     readline.question('Как вас зовут?', name => {
+    
+//     console.log(name);
+    
+//     readline.close();
+    
+// });
+
+
+let x = 5;
+
+let y = 10;
+
+console.log(x > y); // Выведет: false
+
+console.log(x < y); // Выведет: true
+
+console.log(x >= 5); // Выведет: true
+
+console.log(y <= 10); // Выведет: true
